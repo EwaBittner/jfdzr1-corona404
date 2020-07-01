673303e4732f07472ec048dce3fb931a5fcaaac9
Mateusz Adamczyk <mateusz.adamczyk.966@gmail.com>
2020-07-01 21:11:45 +0200

    Added smoothmenu

---
+++ JS/smoothmenu.js
@@ -0,0 +1,12 @@
+
+const menu = document.getElementById("menu")
+
+menu.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
