bd63205da6a1b656b2ab29be0c029b87f3d52a30
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-07-17 16:28:44 +0200

    Popup on hover

--- JS/extra-btn.js
+++ JS/extra-btn.js
@@ -3,3 +3,9 @@ function myFunction() {
     prompt("Ile to 2 x 2?")
     alert("Nie interesuje mnie to:) Nie działa to nie działa:P");
   }
+  $(document).ready(function() {
+    $('[data-toggle="popover"]').popover({
+       placement: 'top',
+       trigger: 'hover'
+    });
+ });
