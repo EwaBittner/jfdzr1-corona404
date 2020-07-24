fb4651b3c4990f36dc176e8c071376eb26292a2b
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-07-24 15:15:15 +0200

    Cookies, design + agree test
    Brak większej funkcjonalności. test głównie wizualny.

---
+++ JS/cookies.js
@@ -0,0 +1,19 @@
+
+//tylko testuje.W przyszłym tyg pokombinuję:)
+
+document.querySelector(".acpt-cookie-no").addEventListener("click", function(){
+
+    document.querySelector('.cookie-container').style.display = "none"
+
+}
+);
+
+
+document.querySelector(".acpt-cookie-yes").addEventListener("click", function(){
+
+    document.querySelector('.cookie-container').style.display = "none"
+
+}
+);
+
+ 
