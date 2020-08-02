83c5581c4bdb6e50038631be928b3f0fcd9340bf
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-08-02 21:13:42 +0200

    Add files via upload
    Zmiana guzika wyślij na Play

--- JS/signIn.js
+++ JS/signIn.js
@@ -3,6 +3,7 @@ const nameInput = document.querySelector('.sign-name');
 const mailInput = document.querySelector('.sign-mail');
 const checkInput = document.querySelector('.sign-check')
 const playBtn = document.querySelector('.play-logo')
+const signInContainer = document.querySelector('.sign-in-container')
 
 console.log(nameInput)
 
@@ -10,9 +11,9 @@ console.log(nameInput)
 const play = sendBtn.addEventListener('click',()=>{
     if(nameInput.value.length && mailInput.value.length && checkInput.checked){
     sendBtn.disabled = true;
+    signInContainer.style.display  = 'none'
     sendBtn.style.display = 'none'
-    playBtn.classList.remove('play-logo');
-    playBtn.classList.add('play-logo-on')
+    playBtn.style.display = ""
 
     
     }
