6cbe3f18f11781828a41360c3c9ac03a703d31e9
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-08-10 10:38:44 +0200

    COR-14
    Rozmiar ekranu nie ma znaczenia, wózek nie wyjedzie poza max

--- MIni-game/player.js
+++ MIni-game/player.js
@@ -5,24 +5,24 @@ checkPositionLeft = ()=>{
 }
 
 checkPositionRight = ()=>{
-    if(parseInt(document.querySelector(".cart").style.left) >= 1170 ){
-        parseInt(document.querySelector(".cart").style.left) = 1170
+    if(parseInt(document.querySelector(".cart").style.left) >= document.querySelector(".game").clientWidth - 195){ // max width minus wiekość wózka
+        parseInt(document.querySelector(".cart").style.left) = document.querySelector(".game").clientWidth - 195; 
     }
 }
 
 movePlayer = ()=>{
-    let start = document.querySelector(".cart").style.left = 600 + 'px';
+    let start = document.querySelector(".cart").style.left = 25 + 'px' //Pozycja startowa wózka
     document.body.onkeydown = () =>{
     const press = event.keyCode,
     Left = parseInt(document.querySelector(".cart").style.left);
 if (press == 37) { 
         checkPositionLeft()
-        document.querySelector(".cart").style.left = parseInt(document.querySelector(".cart").style.left) - 25 + 'px';
+        document.querySelector(".cart").style.left = parseInt(document.querySelector(".cart").style.left) - 25 + 'px'; // o ile ma się przesówać w lewo
         document.querySelector(".cart").style.transform = "scaleX(-1)";
         console.log(document.querySelector(".cart").style.left)
     } else if (press == 39) { 
         checkPositionRight()
-        document.querySelector(".cart").style.left = parseInt(document.querySelector(".cart").style.left) + 25 + 'px';
+        document.querySelector(".cart").style.left = parseInt(document.querySelector(".cart").style.left) + 25 + 'px'; // o ile ma się przesówać w prawo
         document.querySelector(".cart").style.transform = "scaleX(1)";
         console.log(document.querySelector(".cart").style.left)
     } 
