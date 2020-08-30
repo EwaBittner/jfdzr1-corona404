3d65e3cbe233d4a02a5c4c493ec1e805989b1044
Ewa Bittner <bittner.ewa@gmail.com>
2020-08-30 13:53:09 +0200

    COR-25 last changes

--- JS/burger-menu.js
+++ JS/burger-menu.js
@@ -1,14 +1,10 @@
 const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
 const nav = document.querySelector('.site-menu');
 
 menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuOpen = true;
-        menuBtn.classList.add('open');
-        // nav.classList.toggle('.site-menu-active');
+    if(!menuBtn.classList.contains('menu-open')) {
+        menuBtn.classList.add('menu-open');
     } else {
-        menuOpen = false;
-        menuBtn.classList.remove('open');
+        menuBtn.classList.remove('menu-open');
     } 
 });
