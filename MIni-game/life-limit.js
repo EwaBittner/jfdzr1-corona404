84e6a2ba7b83d9aba3b5313b9e4a404c4b068992
maciej-pilarczyk <maciekpilar1@gmail.com>
2020-09-09 14:32:43 +0200

    added function to startagain

--- MIni-game/life-limit.js
+++ MIni-game/life-limit.js
@@ -1,8 +1,9 @@
 const endLifeLost = document.querySelector('.end-life-lost');
-
-
-
+const game1 = document.querySelector('.game');
+const btnStart = document.querySelector('#Start-again');
+const timer3 = document.querySelector('.timer');
 var lifeCounter = 3
+
 
 function lifeLost() {
     lifeCounter--;
@@ -11,8 +12,6 @@ function lifeLost() {
     const icon2 = document.querySelector(".life-icon-2");
     const icon3 = document.querySelector(".life-icon-3");
 
-
-
     if (lifeCounter == 2) {
         icon3.remove()
     } else
@@ -20,11 +19,18 @@ function lifeLost() {
         icon2.remove()
     } else
     if (lifeCounter == 0) {
-        icon1.remove()
-
-        timer.innerHTML = '0:00'
-
+        icon1.remove();
         endLifeLost.style.display = "flex";
+        game1.style.display = "none";
 
     }
 }
+
+let startAgain = btnStart.addEventListener('click', () => {
+    endLifeLost.style.display = "none";
+    game1.style.display = "flex";
+    timer3.innerHTML = '3.00';
+    // fallingObjects()
+
+
+})
